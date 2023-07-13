ee5b021442c4c85f1fa344ba316363f3a2f44768
mihail323i21 <mihail323i21@gmail.com>
2023-07-13 16:45:04 +0500

    fetch data from back

--- api-vk.js
+++ api-vk.js
@@ -1,11 +1,9 @@
-// let post1 = VK.Widgets.Post('vk_post_-22941070_60351', -22941070, 60351, 'tu6YMHbogqhap7VWp0zJ3CKilJo');
-// let post2 = VK.Widgets.Post('vk_post_-22941070_60350', -22941070, 60350, 'ejAJTbUV5s_hA-E3mo_47rTZGS0');
-// let post3 = VK.Widgets.Post('vk_post_-22941070_60344', -22941070, 60344, 'LF0yfWrqrttKRsQUWIw8BaVnwQQ');
-//
-// let postsVK = [
-//     {id: 1, post: post1, ref: 'vk_post_-22941070_60351'},
-//     {id: 2, post: post2, ref: 'vk_post_-22941070_60350'},
-//     {id: 3, post: post3, ref: 'vk_post_-22941070_60344'}
-// ];
-//
-// export {postsVK};
+function getPosts (callback) {
+    fetch('http://26.223.83.180/API/news')
+        .then((response) => response.json())
+        .then(callback);
+}
+
+getPosts((posts) => {
+    console.log(posts);
+});
