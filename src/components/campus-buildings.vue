8925f76a43c3bb93b35f1ae946e78b9dee7fb65d
mihail323i21 <mihail323i21@gmail.com>
2023-07-21 17:22:54 +0500

    data from json server

--- src/components/campus-buildings.vue
+++ src/components/campus-buildings.vue
@@ -6,6 +6,7 @@
 
 <script>
 import BlocksListPage from "@/components/UI/blocks-list-page.vue";
+import axios from "axios";
 
 export default {
   components: {BlocksListPage},
@@ -14,54 +15,17 @@ export default {
   },
   data() {
     return {
-      campus_buildings_articles: [
-        {
-          id: 1,
-          title: 'Студенческое общежитие №11',
-          img_src: "./../../assets/lc_s/lc_rtf.jpg",
-          main_text: 'Студенческое общежитие №11 – общежитие для студентов, находится по адресу ул. Коминтерна, д. 5, б' +
-              'ыло построено в 1961 году, в 2016 был произведен капитальный ремонт. Здание имеет 5 этажей и 220 жилых ко' +
-              'мнат. На каждом этаже есть кухня. На данный момент в общежитии проживает 597 студентов таких институтов,' +
-              ' как ИРИТ-РтФ, ИНМТ, ИнЭУ и ИнФО. В фойе первого этажа любой студент может поиграть в настольный теннис. ' +
-              'С 2012 года открыта комната для занятий. Режим работы: Пн – чт. 8:30-17:15; пт. 8:30-16:00.' +
-              'Совет студенческого корпуса избирается ежегодно студентами, проживающими в общежитии. В функции ССК входит' +
-              ' представление прав и интересов, проживающих перед администрацией университета, привлечение студентов к ' +
-              'решению социально-значимых вопросов, организация воспитательного процесса, досуга и быта проживающих, контроль' +
-              ' за соблюдением правил внутреннего распорядка и нормативно-правовых актов, регулирующих проживание ' +
-              'студентов в общежитии. '
-        },
-        {
-          id: 2,
-          title: 'any title 2',
-          img_src: '',
-          main_text: '123'
-        },
-        {
-          id: 3,
-          title: 'any title 3',
-          img_src: '',
-          main_text: '123'
-        },
-        {
-          id: 4,
-          title: 'any title 4',
-          img_src: '',
-          main_text: '123'
-        },
-        {
-          id: 5,
-          title: 'any title 5',
-          img_src: '',
-          main_text: '123'
-        },
-        {
-          id: 6,
-          title: 'any title 6',
-          img_src: '',
-          main_text: '14124'
-        },
-      ]
+      campus_buildings_articles: [],
     }
+  },
+  methods: {
+    async fetchCampusBuildingArticles() {
+      const response = await axios.get("http://localhost:3000/campus_buildings_articles")
+      this.campus_buildings_articles = response.data
+    }
+  },
+  mounted() {
+    this.fetchCampusBuildingArticles()
   }
 }
 </script>
